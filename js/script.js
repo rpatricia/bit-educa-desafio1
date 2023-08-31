79d45aef0cf3082b5415358b67f279bc71dab854
Renata Pausini <rpatricia@outlook.com>
2023-08-31 18:00:19 -0300

    updated page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-prompt('Olá bem vindo ao biteduca');
+// alert("Olá bem vindo ao biteduca");
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".navbar");
+let header = document.querySelector(".header");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+
+window.addEventListener("scroll",() => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+});
